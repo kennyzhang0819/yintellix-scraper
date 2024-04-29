a6b2c73a110f368d95db9e5eddbeb2ddc4d2896c
Jinlin Zhang <kennyzhang0819@outlook.com>
2024-04-28 17:32:45 -0700

    working version 1

--- linkedin-scraper/linkedin/settings.py
+++ linkedin-scraper/linkedin/settings.py
@@ -12,9 +12,9 @@ BOT_NAME = 'linkedin'
 SPIDER_MODULES = ['linkedin.spiders']
 NEWSPIDER_MODULE = 'linkedin.spiders'
 
-AUTOTHROTTLE_ENABLED = False
+AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_MAX_DELAY = 2
-DOWNLOAD_DELAY = 0.5
+DOWNLOAD_DELAY = 0
 
 USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
 
@@ -26,21 +26,21 @@ ROBOTSTXT_OBEY = False
 SCRAPEOPS_API_KEY = '4009e3f5-6658-4b58-86a6-0e86d02ce488'
 
 SCRAPEOPS_PROXY_ENABLED = False
-# Add In The ScrapeOps Monitoring Extension
-EXTENSIONS = {
-'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
-}
+
+RETRY_HTTP_CODES = [429]
 
 
-# DOWNLOADER_MIDDLEWARES = {
+DOWNLOADER_MIDDLEWARES = {
+    # "linkedin.middlewares.TooManyRequestsRetryMiddleware": 543,
+    # "scrapy.spidermiddlewares.offsite.OffsiteMiddleware": None,
 
 #     ## ScrapeOps Monitor
 #     'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
 #     'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
     
 #     ## Proxy Middleware
-#     'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
-# }
+    # 'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
+}
 
-CONCURRENT_REQUESTS = 64
+CONCURRENT_REQUESTS = 1000
 LOG_LEVEL = "DEBUG"
--- linkedin-scraper/linkedin/spiders/linkedin_company_profile.py
+++ linkedin-scraper/linkedin/spiders/linkedin_company_profile.py
@@ -9,71 +9,73 @@ from scrapy import signals
 from scrapy.signalmanager import dispatcher
 import time
 import re
+import numpy as np
 from collections import Counter
+from bs4 import BeautifulSoup
 
 class GetCompanyLinksSpider(scrapy.Spider):
     name = "linkedin_jobs"
-    api_url = f'https://www.linkedin.com/jobs/search?keywords=Sales%20Development%20Representative&location=United%20States&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&position=12' 
+    api_url = api_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Sales%2BDevelopment%2BRepresentative&location=United%2BStates&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&start='  
     company_links = []
+    max_company = 10
     
     def start_requests(self):
-        index = 0
-        first_url = self.api_url.format(index)
-        yield scrapy.Request(url=first_url, callback=self.parse_job, meta={'first_job_on_page': index})
+        first_job_on_page = 0
+        first_url = self.api_url + str(first_job_on_page)
+        yield scrapy.Request(url=first_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
 
 
     def parse_job(self, response):
         first_job_on_page = response.meta['first_job_on_page']
 
         job_item = {}
-        jobs = response.css(".base-search-card__info")
+        jobs = response.css("li")
 
         num_jobs_returned = len(jobs)
         print("******* Num Jobs Returned *******")
         print(num_jobs_returned)
+        print('*****')
         
         for job in jobs:
             
-            job_link = job.css("a::attr(href)").get(default='not-found').strip()
-            job_title = job.css("h3::text").get(default='not-found').strip()
-            self.company_links.append({'company_link': job_link, 'job_title': job_title})
-            
+            job_item['job_title'] = job.css("h3::text").get(default='not-found').strip()
+            job_item['job_detail_url'] = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
+            job_item['job_listed'] = job.css('time::text').get(default='not-found').strip()
+
+            job_item['company_name'] = job.css('h4 a::text').get(default='not-found').strip()
+            job_item['company_link'] = job.css('h4 a::attr(href)').get(default='not-found')
+            job_item['company_location'] = job.css('.job-search-card__location::text').get(default='not-found').strip()
+            self.company_links.append({'company_name': job_item['company_name'], 'company_link': job_item['company_link']})
         
 
-        if num_jobs_returned > 0 and first_job_on_page < 3:
-            first_job_on_page = first_job_on_page + 1
-            print(first_job_on_page)
-            next_url = self.api_url.format(first_job_on_page)
-            print('***************')
-            print(next_url)
+        if len(self.company_links) < self.max_company:
+            first_job_on_page = int(first_job_on_page) + 25
+            next_url = self.api_url + str(first_job_on_page)
             yield scrapy.Request(url=next_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
         
-        with open('companies.json', 'w') as outfile:
-            json.dump(self.company_links, outfile, indent=4)
+        df = pd.DataFrame(self.company_links)
+        df.drop_duplicates(subset=['company_link'], inplace=True)
+        df.to_csv('companies.csv', index=False)
 
 class GetCompanyProfileSpider(scrapy.Spider):
     name = "linkedin_company_profile"
-    api_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=python&location=United%2BStates&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&start=' 
-
-    #cutstom test
-    company_pages = [
-        'https://www.linkedin.com/company/michael-page/',
-        'https://www.linkedin.com/company/hays/',
-        ]
-
     company_data = []
     size = 0
 
     def start_requests(self):
         dispatcher.connect(self.spider_closed, signals.spider_closed)
-        self.readUrlsFromCompaniesFile()
+        
+        self.company_pages = []
+        df = pd.read_csv('companies.csv')
+        self.company_pages = df['company_link']
+        self.company_pages = list(set(self.company_pages))
         self.size = len(self.company_pages)
         for index, url in enumerate(self.company_pages):
             yield scrapy.Request(url=url, callback=self.parse_response, meta={'index': index})
         
     def parse_response(self, response):
         index = response.meta['index']
-        print('****** progress ' + index / self.size * 100 + '%')
+        print('****** processing ' + str(index) + ' / ' + str(self.size))
 
         company_item = {}
 
@@ -100,32 +102,22 @@ class GetCompanyProfileSpider(scrapy.Spider):
             print("Some details missing for this company. Skipping those details.")
         
         self.company_data.append(company_item)
-
-    def readUrlsFromCompaniesFile(self):
-        self.company_pages = []
-        with open('companies.json') as file:
-            companiesFromFile = json.load(file)
-
-            for company in companiesFromFile:
-                if company['company_link'] != 'not-found':
-                    self.company_pages.append(company['company_link'])
-            
-        self.company_pages = list(set(self.company_pages))
     
     def spider_closed(self, spider):
         df = pd.DataFrame(self.company_data)
         df.to_csv('company_profiles.csv', encoding='utf-8', index=False)
-
 
 class CompanyJobsMatcherSpider(scrapy.Spider):
     name = "linkedin_company_jobs"
     company_job_links = []
     size = 0
     
+    
     def start_requests(self):
         dispatcher.connect(self.spider_closed, signals.spider_closed)
         df = pd.read_csv('company_profiles.csv')
         df = df[["name", "job_link"]]
+        # df['job_link'] = df['job_link'].apply(self.transform_url)
         self.size = len(df)
         for index, url in enumerate(df['job_link']):
             yield scrapy.Request(url=url, callback=self.parse_job, meta={'index': index})
@@ -133,7 +125,7 @@ class CompanyJobsMatcherSpider(scrapy.Spider):
         
     def parse_job(self, response):
         index = response.meta['index']
-        print('****** progress ' + index / self.size * 100 + '%')
+        print('****** processing ' + str(index) + ' / ' + str(self.size))
         jobs = response.css(".base-card")
         
         for job in jobs:
@@ -149,6 +141,13 @@ class CompanyJobsMatcherSpider(scrapy.Spider):
     def spider_closed(self, spider):
         df = pd.DataFrame(self.company_job_links)
         df.to_csv('company_jobs_relation.csv', encoding='utf-8', index=False)
+        
+    def transform_url(self, url):
+        parts = url.split('/')
+        if 'jobs' in parts:
+            new_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/' + parts[-1] + '&start='
+            return new_url
+        return url
 
 class GetJobSpecificsSpider(scrapy.Spider):
     name = "linkedin_company_jobs_specifics"
@@ -161,66 +160,88 @@ class GetJobSpecificsSpider(scrapy.Spider):
         self.size = len(df)
         
         for index, url in enumerate(df['job_link']):
-            yield scrapy.Request(url=url, callback=self.parse_response, meta={'url': url, 'index': index})
+            print("****** requesting " + str(index))
+            yield scrapy.Request(url=url, callback=self.parse_response, meta={'url': url})
         
     def parse_response(self, response):
-        index = response.meta['index']
-        print('****** progress ' + index / self.size * 100 + '%')
         job_specific_item = {}
         
         card_info = response.css('.top-card-layout__entity-info')
         job_specific_item['name'] = card_info.css('h1::text').get(default='not-found').strip()
         job_specific_item['company'] = card_info.css('h4 span a::text').get(default='not-found').strip()
         job_specific_item['job_url'] = response.meta['url']
-        try:
-            job_description = response.css('.core-section-container__content p::text').getall()
-            description = ' '.join(job_description).strip()
-            job_specific_item['description'] = re.sub(r'\s+', ' ', description).strip()
-            
-        except IndexError:
-            print("Some details missing for this company. Skipping those details.")
+
+        soup = BeautifulSoup(response.text, "html.parser")
         
+        salary_div = soup.find('div', class_='compensation__salary')
+        job_specific_item['salary'] = salary_div.get_text(strip=True) if salary_div else "not-found"
+
+        description_section = soup.find('section', class_='show-more-less-html')
+        job_specific_item['description'] = description_section.get_text(strip=True) if description_section else "not-found"
+    
         self.company_job_data.append(job_specific_item)
     
     def spider_closed(self, spider):
         df = pd.DataFrame(self.company_job_data)
         df.to_csv('company_job_specifics.csv', encoding='utf-8', index=False)
 
+
 def calculateStats():
     specifics = pd.read_csv('company_job_specifics.csv')
     profiles = pd.read_csv('company_profiles.csv')
     relation = pd.read_csv('company_jobs_relation.csv')
 
-    # Merging so that all entries in profiles are included
     jobs_with_locations = pd.merge(relation, specifics, how='left', left_on='job_link', right_on='job_url')
-    print(jobs_with_locations.head())
     full_data = pd.merge(profiles, jobs_with_locations, how='left', left_on='name', right_on='company')
+    
+    def parse_and_normalize_salary(salary_str):
+        # Check if the salary field is NaN
+        if pd.isna(salary_str) or salary_str == 'not-found':
+            return None
+        # Find all monetary amounts in the salary string
+        salary_range = re.findall(r'\$\d+,\d+', salary_str)
+        salary_range = [int(x.replace(',', '').replace('$', '')) for x in salary_range]
+        if "/yr" in salary_str:
+            # If salary is annual, use as is
+            annual_salary = salary_range
+        elif "/hr" in salary_str:
+            # If salary is hourly, convert to annual (assuming 2080 hours per year)
+            annual_salary = [x * 2080 for x in salary_range]
+        else:
+            return None
+        # Calculate the mean of the salary range
+        if len(annual_salary) == 2:
+            return np.mean(annual_salary)
+        elif len(annual_salary) == 1:
+            return annual_salary[0]
+        else:
+            return None
 
-    print(full_data.head())
+    full_data['normalized_salary'] = full_data['salary'].apply(parse_and_normalize_salary)
 
     def most_frequent_word(texts):
-        # Common English stop words
-        stop_words = set(["the", "of", "this", "and", "in", "to", "a", "for", "your"])
+        stop_words = set(["the", "of", "this", "and", "in", "to", "a", "for", "your", "you", "or", "we", "with", "be", "our", "on", "is", "are", "not", "all"])
         words = re.findall(r'\b\w+\b', ' '.join(texts).lower())
         words = [word for word in words if word not in stop_words]
         most_common = Counter(words).most_common(1)
         return most_common[0][0] if most_common else None
 
     grouped = full_data.groupby('name_x').agg({
-        'summary': 'first',  # Company description from the first entry of each group
-        'job_location': lambda x: x.value_counts().to_dict() if pd.notna(x).any() else {},  # Distribution of locations
-        'description': lambda x: most_frequent_word(x) if pd.notna(x).any() else None  # Most used word in descriptions
+        'summary': 'first',
+        'job_location': lambda x: x.value_counts().to_dict() if pd.notna(x).any() else {},
+        'description': lambda x: most_frequent_word(x) if pd.notna(x).any() else None,
+        'normalized_salary': ['mean', 'median', 'std', 'max']
     }).reset_index()
 
-    grouped.rename(columns={
-        'name_x': 'Company Name',
-        'summary': 'Company Description',
-        'job_location': 'Location Distribution',
-        'description': 'Most Used Word in Descriptions'
-    }, inplace=True)
+    grouped.columns = [
+        'Company Name', 'Company Description', 'Location Distribution',
+        'Most Used Word in Descriptions', 'Average Salary', 'Median Salary', 
+        'Salary Std Dev', 'Highest Paid Role'
+    ]
 
     grouped.to_csv('final_stats.csv', encoding='utf-8', index=False)
-    
+
+
     
 settings = get_project_settings()
 configure_logging(settings)
@@ -228,10 +249,10 @@ runner = CrawlerRunner(settings)
 
 @defer.inlineCallbacks
 def crawl():
-    # yield runner.crawl(GetCompanyLinksSpider)
-    # yield runner.crawl(GetCompanyProfileSpider)
-    # yield runner.crawl(CompanyJobsMatcherSpider)
-    # yield runner.crawl(GetJobSpecificsSpider)
+    yield runner.crawl(GetCompanyLinksSpider)
+    yield runner.crawl(GetCompanyProfileSpider)
+    yield runner.crawl(CompanyJobsMatcherSpider)
+    yield runner.crawl(GetJobSpecificsSpider)
     calculateStats()
     reactor.stop()
 
